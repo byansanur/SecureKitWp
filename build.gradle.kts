b7085e801dfbc8efee1eff161797f5245517181f
Ratbyansa Nur <byan@sgo.co.id>
2026-08-06 08:39:53 +0700

    Configure JitPack publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,20 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+subprojects {
+    plugins.withId("com.android.library") {
+        apply(plugin = "maven-publish")
+        afterEvaluate {
+            extensions.configure<org.gradle.api.publish.PublishingExtension>("publishing") {
+                publications {
+                    withType<org.gradle.api.publish.maven.MavenPublication>().configureEach {
+                        groupId = "com.github.byansanur.SecureKitWp"
+                        artifactId = project.name
+                        version = "1.0.0"
+                    }
+                }
+            }
+        }
+    }
+}
